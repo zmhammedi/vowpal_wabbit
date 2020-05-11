a5d5d6d509168e0e143aadca47bb518e03503823
Jack Gerrits <jackgerrits95@gmail.com>
2020-05-11 15:38:01 -0400

    Adding initial rough label and prediction apis

--- bindings/c/include/vw/label.h
+++ bindings/c/include/vw/label.h
@@ -11,15 +11,99 @@ extern "C"
 {
 #endif
 
-typedef uint32_t vw_label_type;
-static const vw_label_type vw_label_simple = 0;
+  vw_status allocate_simple_label(vw_simple_label**);
+  vw_status deallocate_simple_label(vw_simple_label*);
 
-vw_status label_allocate(vw_label** label, void* ss, vw_label_type type);
-vw_status label_deallocate(vw_label* label);
+  vw_status simple_label_get_label(vw_simple_label*, float*);
+  vw_status simple_label_set_label(vw_simple_label*, float);
+  vw_status simple_label_get_weight(vw_simple_label*, float*);
+  vw_status simple_label_set_weight(vw_simple_label*, float);
+  vw_status simple_label_get_initial(vw_simple_label*, float*);
+  vw_status simple_label_set_initial(vw_simple_label*, float);
 
-vw_status label_set(vw_label* label, void* ss, vw_label_type type);
-vw_status label_get_value(vw_label* label, void** example);
-vw_status label_get_type(vw_label* label, vw_label_type* type);
+  vw_status allocate_multiclass_label(vw_multiclass_label**);
+  vw_status deallocate_multiclass_label(vw_multiclass_label*);
+  vw_status multiclass_label_get_label(vw_multiclass_label*, uint32_t*);
+  vw_status multiclass_label_set_label(vw_multiclass_label*, uint32_t);
+  vw_status multiclass_label_get_weight(vw_multiclass_label*, float*);
+  vw_status multiclass_label_set_weight(vw_multiclass_label*, float);
+
+  vw_status allocate_cs_label(vw_cs_label**);
+  vw_status deallocate_cs_label(vw_cs_label*);
+  vw_status cs_label_get_length(vw_cs_label*, int*);
+  vw_status cs_label_get_item(
+      vw_cs_label*, int index, float* x, uint32_t* class_index, float* partial_prediction, float* wap_value);
+  vw_status cs_label_set_item(
+      vw_cs_label*, int index, float x, uint32_t class_index, float partial_prediction, float wap_value);
+  vw_status cs_label_push_item(vw_cs_label*, float x, uint32_t class_index, float partial_prediction, float wap_value);
+  vw_status cs_label_remove_item(vw_cs_label*, int index);
+
+  vw_status allocate_cb_label(vw_cb_label**);
+  vw_status deallocate_cb_label(vw_cb_label*);
+  vw_status cb_label_get_length(vw_cb_label*, int*);
+  vw_status cb_label_get_weight(vw_cb_label*, float*);
+  vw_status cb_label_set_weight(vw_cb_label*, float);
+  vw_status cb_label_get_item(
+      vw_cb_label*, int index, float* cost, uint32_t* action, float* probability, float* partial_prediction);
+  vw_status cb_label_set_item(
+      vw_cb_label*, int index, float cost, uint32_t action, float probability, float partial_prediction);
+  vw_status cb_label_push_item(vw_cb_label*, float cost, uint32_t action, float probability, float partial_prediction);
+  vw_status cb_label_remove_item(vw_cb_label*, int index);
+
+  vw_status allocate_ccb_label(vw_ccb_label**);
+  vw_status deallocate_ccb_label(vw_ccb_label*);
+  vw_status ccb_label_get_type(vw_ccb_label*, uint8_t* type);
+  vw_status ccb_label_set_type(vw_ccb_label*, uint8_t type);
+  vw_status ccb_label_get_weight(vw_ccb_label*, float* weight);
+  vw_status ccb_label_set_weight(vw_ccb_label*, float weight);
+  vw_status ccb_label_get_explicit_included_actions(const vw_ccb_label*, uint32_t** indices, int* length);
+  vw_status ccb_label_push_explicit_included_actions(vw_example*, uint32_t value);
+  vw_status ccb_label_remove_explicit_included_actions(vw_example*, int index);
+
+  vw_status allocate_ccb_outcome(vw_ccb_outcome**);
+  vw_status deallocate_ccb_outcome(vw_ccb_outcome*);
+  // may be nullptr
+  vw_status ccb_label_get_outcome(vw_ccb_label*, vw_ccb_outcome** outcome);
+  // may be nullptr
+  vw_status ccb_label_set_outcome(vw_ccb_label*, vw_ccb_outcome* outcome);
+  vw_status ccb_outcome_get_cost(vw_ccb_outcome*, float* cost);
+  vw_status ccb_outcome_set_cost(vw_ccb_outcome*, float cost);
+  vw_status ccb_outcome_get_length(vw_ccb_outcome*, int*);
+  vw_status ccb_outcome_get_item(vw_ccb_outcome*, int index, uint32_t* action, float* score);
+  vw_status ccb_outcome_set_item(vw_ccb_outcome*, int index, uint32_t action, float score);
+  vw_status ccb_outcome_push_item(vw_ccb_outcome*, uint32_t action, float score);
+  vw_status ccb_outcome_remove_item(vw_ccb_outcome*, int index);
+
+  vw_status allocate_slates_label(vw_slates_label**);
+  vw_status deallocate_slates_label(vw_slates_label*);
+  vw_status slates_label_get_type(vw_slates_label*, uint8_t* type);
+  vw_status slates_label_set_type(vw_slates_label*, uint8_t type);
+  vw_status slates_label_get_weight(vw_slates_label*, float* weight);
+  vw_status slates_label_set_weight(vw_slates_label*, float weight);
+  vw_status slates_label_get_cost(vw_slates_label*, float* cost);
+  vw_status slates_label_set_cost(vw_slates_label*, float cost);
+  vw_status slates_label_get_labeled(vw_slates_label*, bool* labeled);
+  vw_status slates_label_set_labeled(vw_slates_label*, bool labeled);
+  vw_status slates_label_get_slot_id(vw_slates_label*, uint32_t* slot_id);
+  vw_status slates_label_set_slot_id(vw_slates_label*, uint32_t slot_id);
+  vw_status slates_label_get_length(vw_slates_label*, int*);
+  vw_status slates_label_get_item(vw_slates_label*, int index, uint32_t* action, float* score);
+  vw_status slates_label_set_item(vw_slates_label*, int index, uint32_t action, float score);
+  vw_status slates_label_push_item(vw_slates_label*, uint32_t action, float score);
+  vw_status slates_label_remove_item(vw_slates_label*, int index);
+
+  vw_status allocate_cb_eval_label(vw_cb_eval_label**);
+  vw_status deallocate_cb_eval_label(vw_cb_eval_label*);
+  vw_status cb_eval_label_get_cb_label(vw_cb_eval_label*, vw_cb_label* cb_label);
+  vw_status cb_eval_label_set_cb_label(vw_cb_eval_label*, vw_cb_label cb_label);
+  vw_status cb_eval_label_get_action(vw_cb_eval_label*, uint32_t* action);
+  vw_status cb_eval_label_set_action(vw_cb_eval_label*, uint32_t action);
+
+  vw_status allocate_multilabels_label(vw_multilabels_label**);
+  vw_status deallocate_multilabels_label(vw_multilabels_label*);
+  vw_status multilabels_label_get(vw_multilabels_label*, uint32_t** multilabels, int* length);
+  vw_status multilabels_label_push(vw_multilabels_label*, uint32_t value);
+  vw_status multilabels_label_remove(vw_multilabels_label*, int index);
 
 #ifdef __cplusplus
 }
--- bindings/c/include/vw/types.h
+++ bindings/c/include/vw/types.h
@@ -16,6 +16,27 @@ static const vw_status vw_success = 0;
 static const vw_status vw_fail = 1;
 static const vw_status vw_not_implemented = 2;
 
+typedef uint32_t vw_label_type;
+static const vw_label_type simple = 0;
+static const vw_label_type cb = 1;
+static const vw_label_type cb_eval = 2;
+static const vw_label_type cs = 3;
+static const vw_label_type multi = 4;
+static const vw_label_type mc = 5;
+static const vw_label_type ccb = 6;
+static const vw_label_type slates = 7;
+
+typedef uint32_t vw_prediction_type;
+static const vw_prediction_type scalar = 0;
+static const vw_prediction_type scalars = 1;
+static const vw_prediction_type action_scores = 2;
+static const vw_prediction_type action_probs = 3;
+static const vw_prediction_type multiclass = 4;
+static const vw_prediction_type multilabels = 5;
+static const vw_prediction_type prob = 6;
+static const vw_prediction_type multiclassprobs = 7;
+static const vw_prediction_type decision_probs = 8;
+
 struct vw_workspace_tag;
 typedef struct vw_workspace_tag vw_workspace;
 
@@ -36,6 +57,36 @@ typedef struct vw_feature_space_tag vw_feature_space;
 
 struct vw_feature_space_ref_tag;
 typedef struct vw_feature_space_ref_tag vw_feature_space_ref;
+
+struct vw_decision_scores_tag;
+typedef struct vw_decision_scores_tag vw_decision_scores;
+
+struct vw_simple_label_tag;
+typedef struct vw_simple_label_tag vw_simple_label;
+
+struct vw_multiclass_label_tag;
+typedef struct vw_multiclass_label_tag vw_multiclass_label;
+
+struct vw_cs_label_tag;
+typedef struct vw_cs_label_tag vw_cs_label;
+
+struct vw_cb_label_tag;
+typedef struct vw_cb_label_tag vw_cb_label;
+
+struct vw_ccb_label_tag;
+typedef struct vw_ccb_label_tag vw_ccb_label;
+
+struct vw_ccb_outcome_tag;
+typedef struct vw_ccb_outcome_tag vw_ccb_outcome;
+
+struct vw_slates_label_tag;
+typedef struct vw_slates_label_tag vw_slates_label;
+
+struct vw_cb_eval_label_tag;
+typedef struct vw_cb_eval_label_tag vw_cb_eval_label;
+
+struct vw_multilabels_label_tag;
+typedef struct vw_multilabels_label_tag vw_multilabels_label;
 
 #ifdef __cplusplus
 }
