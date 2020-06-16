642ca8ba3ee08fb7ad152a9f8731723bc34b4d22
Jack Gerrits <jack@jackgerrits.com>
2020-06-16 12:00:46 -0400

    Convert parser to snake case

--- bindings/c/include/vw/experimental/parser.h
+++ bindings/c/include/vw/experimental/parser.h
@@ -11,19 +11,19 @@ extern "C"
 {
 #endif
 
-  VW_DLL_PUBLIC VWStatus VWCreateExampleArray(
+  VW_DLL_PUBLIC VWStatus vw_create_example_array(
       VWExampleArray** example_array_handle, VWErrorString* err_str_container) VW_API_NOEXCEPT;
-  VW_DLL_PUBLIC VWStatus VWExampleArrayGetContents(
+  VW_DLL_PUBLIC VWStatus vw_example_array_get_contents(
       VWExampleArray* example_array_handle, VWExample**, size_t*, VWErrorString* err_str_container) VW_API_NOEXCEPT;
-  VW_DLL_PUBLIC VWStatus VWDestroyExampleArray(
+  VW_DLL_PUBLIC VWStatus vw_destroy_example_array(
       VWExampleArray* example_array_handle, VWErrorString* err_str_container) VW_API_NOEXCEPT;
 
   // Start the parser+driver loops
-  VW_DLL_PUBLIC VWStatus VWRunParserAndDriver(
+  VW_DLL_PUBLIC VWStatus vw_run_parser_and_driver(
       VWWorkspace* workspace_handle, VWErrorString* err_str_container) VW_API_NOEXCEPT;
 
   // might return a multiline example in the case of the json parser
-  VW_DLL_PUBLIC VWStatus VWParseExample(VWWorkspace* workspace_handle, uint8_t* data, size_t length, VWInputType type,
+  VW_DLL_PUBLIC VWStatus vw_parse_example(VWWorkspace* workspace_handle, uint8_t* data, size_t length, VWInputType type,
       VWExampleArray* results, VWErrorString* err_str_container) VW_API_NOEXCEPT;
 
 #ifdef __cplusplus
