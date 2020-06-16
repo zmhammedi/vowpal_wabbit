5a1efdced54ceee078650df82661e5de1bd1d160
Jack Gerrits <jack@jackgerrits.com>
2020-06-16 12:08:47 -0400

    Convert search  to snake_case

--- bindings/c/include/vw/experimental/search.h
+++ bindings/c/include/vw/experimental/search.h
@@ -15,57 +15,57 @@ extern "C"
   static const uint32_t EXAMPLES_DONT_CHANGE = 4;
   static const uint32_t IS_LDF = 8;
 
-  VW_DLL_PUBLIC VWStatus VWSearchSetOptions(
+  VW_DLL_PUBLIC VWStatus vw_search_set_options(
       VWSearch* search_handle, uint32_t options, VWErrorString* err_str_container) VW_API_NOEXCEPT;
-  VW_DLL_PUBLIC VWStatus VWSearchGetHistoryLength(
+  VW_DLL_PUBLIC VWStatus vw_search_get_history_length(
       VWSearch* search_handle, uint32_t* history_length, VWErrorString* err_str_container) VW_API_NOEXCEPT;
-  VW_DLL_PUBLIC VWStatus VWSearchSetLoss(VWSearch* search_handle, float loss, VWErrorString* err_str_container) VW_API_NOEXCEPT;
-  VW_DLL_PUBLIC VWStatus VWSearchGetShouldOutput(
+  VW_DLL_PUBLIC VWStatus vw_search_set_loss(VWSearch* search_handle, float loss, VWErrorString* err_str_container) VW_API_NOEXCEPT;
+  VW_DLL_PUBLIC VWStatus vw_search_get_should_output(
       VWSearch* search_handle, bool* shouldOutput, VWErrorString* err_str_container) VW_API_NOEXCEPT;
-  VW_DLL_PUBLIC VWStatus VWSearchGetPredictNeedsExample(
+  VW_DLL_PUBLIC VWStatus vw_search_get_predict_needs_example(
       VWSearch* search_handle, bool* shouldOutput, VWErrorString* err_str_container) VW_API_NOEXCEPT;
-  VW_DLL_PUBLIC VWStatus VWSearchOutput(VWSearch* search_handle, const char* output, VWErrorString* err_str_container) VW_API_NOEXCEPT;
-  VW_DLL_PUBLIC VWStatus VWSearchGetNumActions(
+  VW_DLL_PUBLIC VWStatus vw_search_output(VWSearch* search_handle, const char* output, VWErrorString* err_str_container) VW_API_NOEXCEPT;
+  VW_DLL_PUBLIC VWStatus vw_search_get_num_actions(
       VWSearch* search_handle, uint32_t* num_actions, VWErrorString* err_str_container) VW_API_NOEXCEPT;
-  VW_DLL_PUBLIC VWStatus VWSearchSetForceOracle(
+  VW_DLL_PUBLIC VWStatus vw_search_set_force_oracle(
       VWSearch* search_handle, bool forceOracle, VWErrorString* err_str_container) VW_API_NOEXCEPT;
-  VW_DLL_PUBLIC VWStatus VWSearchGetIsLDF(VWSearch* search_handle, bool* isLDF, VWErrorString* err_str_container) VW_API_NOEXCEPT;
-  VW_DLL_PUBLIC VWStatus VWSearchGetPredictor(
+  VW_DLL_PUBLIC VWStatus vw_search_get_is_ldf(VWSearch* search_handle, bool* isLDF, VWErrorString* err_str_container) VW_API_NOEXCEPT;
+  VW_DLL_PUBLIC VWStatus vw_search_get_predictor(
       VWSearch* search_handle, VWSearchPredictor** predictor, uint32_t ptag, VWErrorString* err_str_container) VW_API_NOEXCEPT;
 
   // This is a rough guess as to what is needed here. We will need to test and update as appropriate.
   typedef VWStatus(VWSearchRunFunc)(void*);
   typedef VWStatus(VWSearchSetupFunc)(void*);
   typedef VWStatus(VWSearchTeardownFunc)(void*);
-  VW_DLL_PUBLIC VWStatus VWSearchPredictorSetPredictHooks(VWSearch* search_handle, VWSearchRunFunc* runFunc,
+  VW_DLL_PUBLIC VWStatus vw_search_predictor_set_predict_hooks(VWSearch* search_handle, VWSearchRunFunc* runFunc,
       void* runContext, VWSearchSetupFunc* setupFunc, void* setupContext, VWSearchTeardownFunc* teardownFunc,
       void* teardownContext, VWErrorString* err_str_container) VW_API_NOEXCEPT;
 
-  VW_DLL_PUBLIC VWStatus VWSearchPredictorSetInput(
+  VW_DLL_PUBLIC VWStatus vw_search_predictor_set_input(
       VWSearchPredictor* predictor_handle, VWExample* input, VWErrorString* err_str_container) VW_API_NOEXCEPT;
-  VW_DLL_PUBLIC VWStatus VWSearchPredictorSetInputLength(
+  VW_DLL_PUBLIC VWStatus vw_search_predictor_set_input_length(
       VWSearchPredictor* predictor_handle, size_t inputLength, VWErrorString* err_str_container) VW_API_NOEXCEPT;
-  VW_DLL_PUBLIC VWStatus VWSearchPredictorSetInputAt(
+  VW_DLL_PUBLIC VWStatus vw_search_predictor_set_input_at(
       VWSearchPredictor* predictor_handle, size_t position, VWExample* input, VWErrorString* err_str_container) VW_API_NOEXCEPT;
-  VW_DLL_PUBLIC VWStatus VWSearchPredictorPushOracle(
+  VW_DLL_PUBLIC VWStatus vw_search_predictor_push_oracle(
       VWSearchPredictor* predictor_handle, uint32_t oracle, VWErrorString* err_str_container) VW_API_NOEXCEPT;
-  VW_DLL_PUBLIC VWStatus VWSearchPredictorClearOracle(
+  VW_DLL_PUBLIC VWStatus vw_search_predictor_clear_oracle(
       VWSearchPredictor* predictor_handle, VWErrorString* err_str_container) VW_API_NOEXCEPT;
-  VW_DLL_PUBLIC VWStatus VWSearchPredictorPushAllowed(
+  VW_DLL_PUBLIC VWStatus vw_search_predictor_push_allowed(
       VWSearchPredictor* predictor_handle, uint32_t allowed, VWErrorString* err_str_container) VW_API_NOEXCEPT;
-  VW_DLL_PUBLIC VWStatus VWSearchPredictorClearAllowed(
+  VW_DLL_PUBLIC VWStatus vw_search_predictor_clear_allowed(
       VWSearchPredictor* predictor_handle, VWErrorString* err_str_container) VW_API_NOEXCEPT;
-  VW_DLL_PUBLIC VWStatus VWSearchPredictorPushCondition(
+  VW_DLL_PUBLIC VWStatus vw_search_predictor_push_condition(
       VWSearchPredictor* predictor_handle, uint32_t tag, char name, VWErrorString* err_str_container) VW_API_NOEXCEPT;
-  VW_DLL_PUBLIC VWStatus VWSearchPredictorPushConditionRange(
+  VW_DLL_PUBLIC VWStatus vw_search_predictor_push_condition_range(
       VWSearchPredictor* predictor_handle, uint32_t hi, uint32_t cout, char name0, VWErrorString* err_str_container) VW_API_NOEXCEPT;
-  VW_DLL_PUBLIC VWStatus VWSearchPredictorClearCondition(
+  VW_DLL_PUBLIC VWStatus vw_search_predictor_clear_condition(
       VWSearchPredictor* predictor_handle, VWErrorString* err_str_container) VW_API_NOEXCEPT;
-  VW_DLL_PUBLIC VWStatus VWSearchPredictorSetLearnerID(
+  VW_DLL_PUBLIC VWStatus vw_search_predictor_set_learner_id(
       VWSearchPredictor* predictor_handle, size_t id, VWErrorString* err_str_container) VW_API_NOEXCEPT;
-  VW_DLL_PUBLIC VWStatus VWSearchPredictorSetTag(
+  VW_DLL_PUBLIC VWStatus vw_search_predictor_set_tag(
       VWSearchPredictor* predictor_handle, uint32_t tag, VWErrorString* err_str_container) VW_API_NOEXCEPT;
-  VW_DLL_PUBLIC VWStatus VWSearchPredictorPredict(
+  VW_DLL_PUBLIC VWStatus vw_search_predictor_predict(
       VWSearchPredictor* predictor_handle, uint32_t* action, VWErrorString* err_str_container) VW_API_NOEXCEPT;
 
 #ifdef __cplusplus
