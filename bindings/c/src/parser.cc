644a42eca0a8ac9a6baec017ef79eddd439a7906
Jack Gerrits <jack@jackgerrits.com>
2020-07-10 15:42:51 -0400

    Update stubs to match declarations

--- bindings/c/src/parser.cc
+++ bindings/c/src/parser.cc
@@ -5,25 +5,16 @@
 #include "vw/experimental/parser.h"
 
 #include "error_handling.h"
-
-VW_DLL_PUBLIC VWStatus vw_create_example_array(
-    VWExampleArray** example_array_handle, VWErrorString* err_str_container) noexcept
+VW_DLL_PUBLIC VWStatus vw_create_example_array_holder(VWExampleArrayHolder** example_array_holder_handle,
+    VWErrorString* err_str_container, void* factory_context, VWExampleFactoryFunc* factory, void* cleanup_context,
+    VWExampleCleanupFunc* cleanup) noexcept
 try
 {
   return VW_NOT_IMPLEMENTED;
 }
 CATCH_RETURN(err_str_container)
-
-VW_DLL_PUBLIC VWStatus vw_example_array_get_contents(
-    VWExampleArray* example_array_handle, VWExample**, size_t*, VWErrorString* err_str_container) noexcept
-try
-{
-  return VW_NOT_IMPLEMENTED;
-}
-CATCH_RETURN(err_str_container)
-
-VW_DLL_PUBLIC VWStatus vw_destroy_example_array(
-    VWExampleArray* example_array_handle, VWErrorString* err_str_container) noexcept
+VW_DLL_PUBLIC VWStatus vw_destroy_example_array_holder(
+    VWExampleArrayHolder* example_array_holder_handle, VWErrorString* err_str_container) noexcept
 try
 {
   return VW_NOT_IMPLEMENTED;
@@ -40,9 +31,9 @@ try
 CATCH_RETURN(err_str_container)
 
 // Might return a multiline example in the case of the json parser
-VW_DLL_PUBLIC VWStatus vw_parse_example(VWWorkspace* workspace_handle, uint8_t* data, size_t length, VWInputType type,
-    VWExampleArray* results, void* factory_context, VWExampleFactoryFunc* factory,
-    VWErrorString* err_str_container) noexcept
+VW_DLL_PUBLIC VWStatus vw_parse_example(VWWorkspace* workspace_handle, /*const*/ uint8_t* data, size_t length,
+    VWInputType type, VWExampleArrayHolder* example_array_holder_handle, VWExample*** produced_examples,
+    size_t* num_examples_produced, VWExampleFactoryFunc* factory, VWErrorString* err_str_container) noexcept
 try
 {
   return VW_NOT_IMPLEMENTED;
