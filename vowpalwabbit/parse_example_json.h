dbf5ae7cad93548ccabf15ac44ea87699df4ea29
Alexey Taymanov <ataymano@gmail.com>
2021-06-09 19:19:50 -0400

    build / tests fix

--- vowpalwabbit/parse_example_json.h
+++ vowpalwabbit/parse_example_json.h
@@ -1662,13 +1662,8 @@ void read_line_json(vw& all, v_array<example*>& examples, char* line, example_fa
   read_line_json_s<audit>(all, examples, line, strlen(line), example_factory, ex_factory_context, dedup_examples);
 }
 
-inline bool apply_pdrop(vw& all, float pdrop, v_array<example*>& examples)
+inline void apply_pdrop(vw& all, float pdrop, v_array<example*>& examples)
 {
-  if (pdrop == 1.)
-  {
-    logger::errlog_error("JSON parser error: examples with pdrop==1 are not supported");
-    return false;
-  }
   // Event with certain pdrop had (1-pdrop) as probability to survive,
   // so it is one of (1 / (1-pdrop)) events that we should learn on, and weight should be updated accordingly.
   if (all.example_parser->lbl_parser.label_type == label_type_t::cb)
@@ -1683,7 +1678,6 @@ inline bool apply_pdrop(vw& all, float pdrop, v_array<example*>& examples)
   {
     // TODO
   }
-  return true;
 }
 
 template <bool audit>
